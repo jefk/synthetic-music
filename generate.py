8bec165f99316962cc32982d51c0d9915109870e
jefk <speedracer111@gmail.com>
2016-01-14 11:33:47 -0800

    more generalized variable names

--- generate.py
+++ generate.py
@@ -1,59 +1,45 @@
-'''Example script to generate text from Nietzsche's writings.
-
-At least 20 epochs are required before the generated text
-starts sounding coherent.
-
-It is recommended to run this script on GPU, as recurrent
-networks are quite computationally intensive.
-
-If you try this script on new data, make sure your corpus
-has at least ~100k characters. ~1M is better.
-'''
-
 from __future__ import print_function
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten
-from keras.datasets.data_utils import get_file
 from keras.optimizers import SGD
 import numpy as np
 import random
 import sys
 
 def with_training(training_data):
-    text = training_data
-    print('corpus length:', len(text))
+    print('corpus length:', len(training_data))
 
-    chars = set(text)
-    print('total chars:', len(chars))
-    char_indices = dict((c, i) for i, c in enumerate(chars))
-    indices_char = dict((i, c) for i, c in enumerate(chars))
+    tokens = set(training_data) # set of all notes
+    print('total tokens:', len(tokens))
+    token_indexes = dict((c, i) for i, c in enumerate(tokens))
+    indexes_token = dict((i, c) for i, c in enumerate(tokens))
 
-    # cut the text in semi-redundant sequences of maxlen characters
+    # cut the training_data in semi-redundant sequences of maxlen characters
     maxlen = 20
     step = 1
     sentences = []
-    next_chars = []
-    for i in range(0, len(text) - maxlen, step):
-        sentences.append(text[i: i + maxlen])
-        next_chars.append(text[i + maxlen])
+    next_tokens = []
+    for i in range(0, len(training_data) - maxlen, step):
+        sentences.append(training_data[i: i + maxlen])
+        next_tokens.append(training_data[i + maxlen])
     print('nb sequences:', len(sentences))
 
     print('Vectorization...')
-    X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
-    y = np.zeros((len(sentences), len(chars)), dtype=np.bool)
+    X = np.zeros((len(sentences), maxlen, len(tokens)), dtype=np.bool)
+    y = np.zeros((len(sentences), len(tokens)), dtype=np.bool)
     for i, sentence in enumerate(sentences):
         for t, char in enumerate(sentence):
-            X[i, t, char_indices[char]] = 1
-        y[i, char_indices[next_chars[i]]] = 1
+            X[i, t, token_indexes[char]] = 1
+        y[i, token_indexes[next_tokens[i]]] = 1
 
 
     # build the model
     print('Build model...')
     model = Sequential()
-    model.add(Flatten(input_shape=(maxlen, len(chars))))
+    model.add(Flatten(input_shape=(maxlen, len(tokens))))
     model.add(Dense(64, init="glorot_uniform"))
     model.add(Activation('tanh'))
-    model.add(Dense(output_dim=len(chars), init="glorot_uniform"))
+    model.add(Dense(output_dim=len(tokens), init="glorot_uniform"))
     model.add(Activation('softmax'))
 
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
@@ -69,25 +55,25 @@ def with_training(training_data):
     print('-' * 50)
     model.fit(X, y, batch_size=16, nb_epoch=100)
 
-    start_index = random.randint(0, len(text) - maxlen - 1)
+    start_index = random.randint(0, len(training_data) - maxlen - 1)
 
     diversity = 0.75
     print()
     print('----- diversity:', diversity)
 
-    sentence = text[start_index: start_index + maxlen - 1]
+    sentence = training_data[start_index: start_index + maxlen - 1]
     generated = sentence
     print('----- Generating with seed: {sentence!s}'.format(**locals()))
     print()
 
     for i in range(100):
-        x = np.zeros((1, maxlen, len(chars)))
+        x = np.zeros((1, maxlen, len(tokens)))
         for t, char in enumerate(sentence):
-            x[0, t, char_indices[char]] = 1.
+            x[0, t, token_indexes[char]] = 1.
 
         preds = model.predict(x, verbose=0)[0]
         next_index = sample(preds, diversity)
-        next_char = indices_char[next_index]
+        next_char = indexes_token[next_index]
 
         generated.append(next_char)
         sentence = sentence[1:]
--- run.py
+++ run.py
@@ -4,5 +4,4 @@ import write_midi
 
 training_data = read_midi.to_int_array('music_data/cs1-1pre.mid')
 notes = generate.with_training(training_data)
-print notes
 write_midi.from_array(notes)
